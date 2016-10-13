5193400e9e1b661e2288b2c44fc9c8415344fa84
raghunandan <raghunandan.gupta@exclusively.com>
2016-10-13 23:42:14 +0530

    complete setup added.

--- src/main/scala/com/demo/MainExample.scala
+++ src/main/scala/com/demo/MainExample.scala
@@ -1,4 +1,4 @@
-package com.sparkscala
+package com.demo
 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
@@ -7,7 +7,7 @@ import org.apache.spark.SparkContext._
 object MainExample {
 
   def main(arg: Array[String]) {
-    print("lolll-ll") 
+    print("lolll-lll") 
     val jobName = "MainExample"
     val conf = new SparkConf().setAppName(jobName).setMaster("local[2]")
     val sc = new SparkContext(conf)
