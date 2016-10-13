43d7f612858605ebc63ec0649267986c589cfe36
raghunandan <raghunandan.gupta@exclusively.com>
2016-10-14 00:14:20 +0530

    Code fixed.

--- src/main/scala/com/demo/MainExample.scala
+++ src/main/scala/com/demo/MainExample.scala
@@ -7,12 +7,11 @@ import org.apache.spark.SparkContext._
 object MainExample {
 
   def main(arg: Array[String]) {
-    print("lolll-lll") 
     val jobName = "MainExample"
     val conf = new SparkConf().setAppName(jobName).setMaster("local[2]")
     val sc = new SparkContext(conf)
     val textFile = sc.textFile("/home/raghunandangupta/inputfiles/TravelData.txt")
-    val split = textFile.map(lines => lines.split('\t')).map(x => (x(2), 1)).reduceByKey(_ + _).map(item => item.swap).sortByKey(false).take(20)
-    split.foreach(println)
+    val split = textFile.map(lines => lines.split('\t')).map(x => (x(2), 1)).reduceByKey(_ + _).map(item => item.swap)
+    split.foreach(println) 
   }
 }
